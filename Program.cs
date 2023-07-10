afec994183e090c88a712136f987cb09fa913042
Nicolas <Nicolas@DESKTOP-JTJB4PF>
2023-07-10 22:46:48 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+using WebApi_DaData;
+
+Console.WriteLine("Введите ИНН Организации для проверки");
+Console.WriteLine("'q' или 'quit' для выхода");
+
+string inputVal = Console.ReadLine()
+    .ToLower();
+
+DaDataHelper daData = new DaDataHelper();
+
+while (inputVal != "q" && inputVal != "quit")
+{
+    var info = daData.GetInfoForINN(inputVal).Result;
+
+    Console.WriteLine(info);
+
+    inputVal = Console.ReadLine()
+    .ToLower();
+
+
+}
+
+Console.WriteLine("До Свидания, Пользователь.");
